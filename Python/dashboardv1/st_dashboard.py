5b29dd59be746be97c2cb3f2041c228765681c47
betaigeuze <linoventura1@hotmail.de>
2022-05-02 14:49:42 +0200

    minor change

--- Python/dashboardv1/st_dashboard.py
+++ Python/dashboardv1/st_dashboard.py
@@ -25,15 +25,31 @@ def main():
     dc = DashboardController(melbourne_data, melbourne_features)
     dc.create_base_dashboard()
     # TODO:
-    # Find a way to connect the indice values of the RF overview with the actual
-    # estimators of the model.
-    # Also:
-    # Think of some interesting sorting criteria for the trees
-    # Then:
-    # Add interactive selection of the trees (this does not make sense, if neither of the
-    # 2 points above are solved.)
-    dc.create_RF_overview()
-    dc.feature_importance_barchart(rfm.model.feature_importances_)
+    # Fix selection interval to work on other charts
+    # Find a way that is scalable for many charts on the dashboard
+    # (reference this: https://altair-viz.github.io/altair-tutorial/notebooks/06-Selections.html)
+    tree_df = get_tree_df_from_model(rfm)
+
+    # create scatter returns the filtered altair selection interval object
+    scatter_filter_interval = dc.create_scatter(tree_df)
+    dc.feature_importance_barchart(
+        rfm.model.feature_importances_, scatter_filter_interval
+    )
+
+    # deprecated:
+    # dc.create_RF_overview()
+
+
+# Inspect RF trees and retrieve number of leaves and depth for each tree
+# This could be altered to more interesting metrics in the future
+def get_tree_df_from_model(rfm):
+    tree_df = pd.DataFrame()
+    for est in rfm.model.estimators_:
+        new_row = {"n_leaves": est.get_n_leaves(), "depth": est.get_depth()}
+        # TODO:
+        # regarding pandas warning => replace append with concat
+        tree_df = tree_df.append(new_row, ignore_index=True)
+    return tree_df
 
 
 if __name__ == "__main__":
