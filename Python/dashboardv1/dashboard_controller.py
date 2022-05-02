1661357fc219c6932080b61fc5d7fb357b4d4c6b
betaigeuze <linoventura1@hotmail.de>
2022-05-02 14:49:00 +0200

    Added new scatter chart and selection window

--- Python/dashboardv1/dashboard_controller.py
+++ Python/dashboardv1/dashboard_controller.py
@@ -10,14 +10,19 @@ class DashboardController:
         self.dashboard_sidebar.title("Sidebar")
         self.dashboard_sidebar.markdown("# Sidebar")
         self.dashboard = st.container()
-
+        st.header("RaFoView")
         self.dataset = dataset
         self.features = features
 
     def create_base_dashboard(self):
-        st.write(self.dataset.head())
+        st.subheader("Input Data")
+        # st.write(self.dataset.head())
 
-    def feature_importance_barchart(self, feature_importances_: pd.DataFrame):
+    def feature_importance_barchart(
+        self,
+        feature_importances_: pd.DataFrame,
+        scatter_interval: alt.selection_interval,
+    ):
         importances = list(feature_importances_)
         # List of tuples with variable and importance
         feature_importance = [
@@ -33,9 +38,28 @@ class DashboardController:
             alt.Chart(feature_importance_df)
             .mark_bar(opacity=0.3)
             .encode(x="1:Q", y=alt.Y("0:N", stack=None, sort="-x"))
-        )
+        ).transform_filter(scatter_interval)
         st.altair_chart(chart, use_container_width=True)
 
+    """Scatterplot displaying all estimators of the RF model
+    x-Axis: number of leaves
+    y-Axis: depth of the tree"""
+
+    def create_scatter(self, tree_df: pd.DataFrame):
+        interval = alt.selection_interval()
+        chart = (
+            alt.Chart(tree_df)
+            .mark_point()
+            .encode(
+                x=alt.X("n_leaves", scale=alt.Scale(zero=False)),
+                y=alt.Y("depth", scale=alt.Scale(zero=False)),
+            )
+            .add_selection(interval)
+        )
+        st.altair_chart(chart, use_container_width=True)
+        return interval
+
+    # Deprecated and replaced by create_scatter
     def create_RF_overview(self):
         a = range(10)
         b = range(10)
@@ -46,6 +70,9 @@ class DashboardController:
         chart = (
             alt.Chart(product_df)
             .mark_circle(size=60)
-            .encode(x="latitude:N", y="longitude:N",)
+            .encode(
+                x="latitude:N",
+                y="longitude:N",
+            )
         )
         st.altair_chart(chart, use_container_width=True)
