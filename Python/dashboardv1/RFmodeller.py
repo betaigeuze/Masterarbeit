9574374bcfaeb9c7f59126f070c8fea9549b2264
betaigeuze <linoventura1@hotmail.de>
2022-05-22 18:47:12 +0200

    Parallelization works. Unparallelized reference

--- Python/dashboardv1/RFmodeller.py
+++ Python/dashboardv1/RFmodeller.py
@@ -4,6 +4,7 @@ from sklearn import tree
 from sklearn.cluster import DBSCAN, KMeans, AgglomerativeClustering
 from scipy.spatial.distance import pdist
 from collections import deque
+import multiprocessing as mp
 import pandas as pd
 import networkx as nx
 import numpy as np
@@ -32,7 +33,7 @@ class RFmodeller:
             X, y, random_state=0, test_size=0.25
         )
         forest_model = RandomForestRegressor(
-            n_estimators=10, random_state=1, max_depth=4, n_jobs=-1
+            n_estimators=50, random_state=1, max_depth=4, n_jobs=-1
         )
         forest_model.fit(train_X, train_y)
         return forest_model, train_X, val_X, train_y, val_y
@@ -65,23 +66,50 @@ class RFmodeller:
         # We also can not use the scipy distance matrix method because it needs
         # vector inputs, but we only have 2 graph objects.
 
-        distance_matrix = np.asarray(
-            [
-                [
-                    deque(
-                        nx.optimize_graph_edit_distance(p1, p2, upper_bound=10.0),
-                        maxlen=1,
-                    ).pop()
-                    if p1 != p2
-                    else 0.0
-                    for p2 in self.directed_graphs
-                ]
-                for p1 in self.directed_graphs
-            ]
-        )
+        # Run the calc_dist_matrix method in parallel.
+        # I used this idea:
+        # https://stackoverflow.com/a/56038389/12355337
+        # Every process gets a slice of the list of graphs.
+        with mp.Pool() as pool:
+            distance_matrix = np.array(
+                pool.map(self.calc_dist_matrix_parallel, self.directed_graphs)
+            )
+
+        # OUTDATED, Only here for reference
+        # Unparallelized:
+        # distance_matrix = self.calc_dist_matrix(self.directed_graphs)
 
         # calculate clusters
         clustering = AgglomerativeClustering(
             affinity="precomputed", linkage="average"
         ).fit(distance_matrix)
         return clustering
+
+    def calc_dist_matrix_parallel(self, directed_graph: nx.DiGraph) -> np.ndarray:
+        # This is still not optimal, because every row and column value is being calculated
+        # It would be possible (and smarter) to calculate half the matrix and then mirror it.
+        # However, this is difficult, because of the calculations happening in parallel.
+        dist_matrix = np.zeros(len(self.directed_graphs))
+        for i, graph1 in enumerate(self.directed_graphs):
+            if graph1 != directed_graph:
+                result_generator = nx.optimize_edit_paths(
+                    graph1, directed_graph, timeout=1
+                )
+                dist_matrix[i] = deque(result_generator, maxlen=1).pop()[2]
+            else:
+                dist_matrix[i] = 0
+        return dist_matrix
+
+    # OUTDATED, only here for reference
+    def calc_dist_matrix(self, directed_graphs: list[nx.DiGraph]) -> np.ndarray:
+        dist_matrix = np.empty([len(directed_graphs)] * 2, nx.DiGraph)
+        for i, graph1 in enumerate(directed_graphs):
+            for j, graph2 in enumerate(directed_graphs):
+                if graph1 != graph2:
+                    dist_matrix[i, j] = deque(
+                        nx.optimize_graph_edit_distance(graph1, graph2),
+                        maxlen=1,
+                    ).pop()
+                else:
+                    dist_matrix[i, j] = 0
+        return dist_matrix
